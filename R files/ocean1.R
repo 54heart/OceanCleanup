34ae577cf215bc415ae699e41261988393718908
54heart <cindy_lh_chang@yahoo.com>
2020-02-22 14:38:43 -0500

    Update ocean1.R

--- R files/ocean1.R
+++ R files/ocean1.R
@@ -3,7 +3,7 @@ raw_data <- read_excel("~/Downloads/Data_Level5_BAH_OceanCleanup.xlsx")
 library(plotly)
 library(dplyr)
 library(tidyverse)
-
+library(ggplot2)
 
 plot(x = raw_data$State, y = )
 
@@ -11,23 +11,34 @@ plot(x = raw_data$State, y = )
 
 df = raw_data[, -1] # remove the ID column
 df = unique(df) # get unique rows
-write.csv(df, "~/Downloads/clean1.csv") # export csv
+# write.csv(df, "~/Documents/GitHub/OceanCleanup/Cleaned/clean4.csv") # export csv
 
 # import cleaned data
-df <- read_csv("~/Documents/GitHub/OceanCleanup/Cleaned/clean3.csv", 
-                   col_types = cols(Adults = col_integer(), 
-                                    Children = col_integer(), `Group Name` = col_character(), 
-                                    People = col_integer()))
+# df <- read_csv("~/Documents/GitHub/OceanCleanup/Cleaned/clean3.csv", 
+                  # col_types = cols(Adults = col_integer(), 
+                                  #  Children = col_integer(), `Group Name` = col_character(), 
+                                   # People = col_integer()))
+
 
 ### convert data type
 df[, 11:60] <- sapply(df[, 11:60], as.integer)
 sapply(df, class) # view the data type 
-write.csv(df, "~/Documents/GitHub/OceanCleanup/Cleaned/clean4.csv") # export csv
 
 
+
+df %>%
+  select(Year<=2014)
+
+# 
+df %>% 
+  select(Zone, Country)
 
 df %>% 
-  select(FL_DATE, CARRIER, ORIGIN, ORIGIN_CITY_NAME, ORIGIN_STATE_ABR, DEP_DELAY, DEP_TIME, ARR_DELAY, ARR_TIME)
 
 
+# Plots
+boxplot(df$`Total Items Collected`)
+barplot(df$State, df$`Total Items Collected`)
 
+ggplot(data = df) +
+  geom_bar(mapping = aes(x = Zone))
